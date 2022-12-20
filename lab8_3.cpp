0cc25e9ab69608d5c3c4d6a7da4d39fb25a7b35e
xchalarm <tharana4671@gmail.com>
2022-12-20 15:28:51 +0700

    lab8_3 completed

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,19 +1,18 @@
-#include<iostream>
+
 using namespace std;
-
 char before(char x){
-	//Write your function definition here
+    if(x == 'A'){
+        return  'Z' ;
+    }
+    
+    if('A'<x and x<='Z'){
+        return x-1;
+    }
+    else if('a'<x and x<'z'){
+        return '0';
+    }
+    else{ 
+        return '0' ;
+    }
 }
 
-int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
-}
